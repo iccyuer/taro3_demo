5396ada82b21dcbea05c06e10b2c6fb9e7c1eeb4
yankuan <yankuan@kongzhong.com>
2022-01-17 14:01:32 +0800

    update

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -3,10 +3,10 @@ import { View, Button, Text } from '@tarojs/components'
 import { observer, inject } from 'mobx-react'
 import basePage from '../../utils/basePage'
 import './index.less'
-
+import Taro from "@tarojs/taro";
 
 // @inject('store')
-@inject(({store}, props) => ({
+@inject(({ store }, props) => ({
   userStore: store.userStore
 }))
 @observer
@@ -17,6 +17,7 @@ class Index extends basePage {
     let { userInfo } = this.props.userStore;
     console.log('userInfo', userInfo)
     console.log('profile componentWillMount')
+    this.getUserLevel(userInfo.token)
   }
 
   componentDidMount() { }
@@ -26,6 +27,24 @@ class Index extends basePage {
   componentDidShow() { }
 
   componentDidHide() { }
+
+  async getUserLevel(token) {
+    let results = await Taro.request({
+      url: 'https://hbdapi.jshbc2017.com/baouser/user/getUserLevel', //仅为示例，并非真实的接口地址
+      data: {},
+      method: 'post',
+      header: {
+        'content-type': 'application/json', // 默认值
+        'authorization': token
+      },
+    }).catch(() => {
+      Taro.showToast({
+        icon: "none",
+        title: results.msg || "网络异常，请稍后重试"
+      });
+    });
+    console.log('user', results.data)
+  }
 
   render() {
     return (
